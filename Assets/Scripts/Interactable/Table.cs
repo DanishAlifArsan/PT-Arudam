a2b03c1396e8d40750899415e8d03863ca8bb5b3
danish <danishalif16@gmail.com>
2024-07-12 14:11:30 +0700

    refactor code for place item on table

--- Assets/Scripts/Interactable/Table.cs
+++ Assets/Scripts/Interactable/Table.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-//to do: benerin kode biar performa bagus
 public class Table : MonoBehaviour
 {
     Grid grid;
@@ -12,62 +12,39 @@ public class Table : MonoBehaviour
     }
 
     public void PlaceItem(Item item) {
-        for (int i = 0; i < grid.width; i++)
+        for (int i = 0; i < grid.dictionary.Count; i++)
         {
-            for (int j = 0; j < grid.height; j++)
+            if (grid.dictionary[grid.dictionary.ElementAt(i).Key] == null)
             {
-                if (grid[i,j].Value == null)
-                {
-                    grid[i, j] = new KeyValuePair<Vector2, Item>( grid[i, j].Key, item);
-                    item.transform.parent = transform;
-                    item.table = this;
-                    item.transform.localPosition = new Vector3(grid[i, j].Key.y, 0f, grid[i, j].Key.x) ;
-                    item.transform.localRotation = Quaternion.identity;
-                    item.EnableHighlight(true);
-                    return;
-                }
+                item.transform.parent = transform;
+                item.transform.localPosition = grid.dictionary.ElementAt(i).Key;
+                item.transform.localRotation = Quaternion.identity;
+                item.table = this;
+                item.EnableHighlight(true);
+                grid.dictionary[grid.dictionary.ElementAt(i).Key] = item;
+                break;      
             }
         }
     }
 
     public void RemoveItem(Item item) {
-        for (int i = 0; i < grid.width; i++)
-        {
-            for (int j = 0; j < grid.height; j++)
-            {
-                if (grid[i,j].Value == item)
-                {
-                    grid[i,j] = new KeyValuePair<Vector2, Item>(grid[i,j].Key, null);
-                    item.table = null;
-                    break;
-                }
-            }
-        }
+        grid.dictionary[item.transform.localPosition] = null;
+        item.storage = null;
     }
 
     public bool IsGridNull() {
-        for (int i = 0; i < grid.width; i++)
-        {
-            for (int j = 0; j < grid.height; j++)
-            {
-                if (grid[i,j].Value == null)
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return grid.dictionary.ContainsValue(null);
     }
 }
 
 class Grid{
-    private static KeyValuePair<Vector2, Item>[,] _grid;
+    public Dictionary<Vector3, Item> dictionary;
     public int width;
     public int height;
     public Grid(int width, int height) {
         this.width = width;
         this.height = height;
-        _grid = new KeyValuePair<Vector2, Item>[width,height];
+        dictionary = new Dictionary<Vector3, Item>();
 
         float xLength = 0.18f + 0.5f;
         float xColumnLength = xLength / width;
@@ -79,23 +56,9 @@ class Grid{
         {
             for (int j = 0; j < height; j++)
             {
-                Vector2 pos = new Vector2(xColumnLength * i - 0.5f , yColumnLength * j + 0.6f);
-                _grid[i,j] = new KeyValuePair<Vector2, Item>(pos, null);
+                Vector3 pos = new Vector3(yColumnLength * j + 0.6f, 0f, xColumnLength * i - 0.5f);
+                dictionary.Add(pos, null);
             }
         }
     }
-
-    public KeyValuePair<Vector2, Item> this[int i, int j]
-    {
-        get
-        {
-            return _grid[i, j];
-        }
-        set
-        {
-            _grid[i, j] = value;
-        }
-    }
 }
-
-
