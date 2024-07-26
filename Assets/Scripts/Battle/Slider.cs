4c769885030b8624ec756e82cfcc1f6a84de3f06
danish <danishalif16@gmail.com>
2024-07-26 13:29:23 +0700

    create qte bar

---
+++ Assets/Scripts/Battle/Slider.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Slider : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 start;
+    [SerializeField] private Vector3 end;
+    private bool colliderFlag = false;
+    private Status sliderStatus;
+
+    enum Status
+    {
+        Full,
+        Half,
+        Miss,
+    }
+
+    private void FixedUpdate() {
+        float pinPosX = transform.localPosition.x + Time.deltaTime * speed;
+        transform.localPosition = new Vector2(pinPosX, transform.localPosition.y);
+
+        if (transform.localPosition.x > end.x)
+        {
+            ResetPosition();
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && colliderFlag){
+            ResetPosition();
+        }
+    }
+
+    private void ResetPosition() {
+        Debug.Log(sliderStatus);
+        transform.localPosition = start;
+        colliderFlag = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        switch (other.tag)
+           {
+            case "Full":
+            sliderStatus = Status.Full;
+            colliderFlag = true;
+                break;
+            case "Half":
+            sliderStatus = Status.Half;
+            colliderFlag = true;
+                break;
+            default:
+            sliderStatus = Status.Miss;
+            colliderFlag = true;
+                break;
+           }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        colliderFlag = false;
+    }
+}
