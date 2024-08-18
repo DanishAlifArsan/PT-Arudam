02b8553d66c0cdac4ce3ac10ca9e2a7d12eab6dc
danish <danishalif16@gmail.com>
2024-08-18 15:08:26 +0700

    localization on phone

--- Assets/Scripts/UI/Phone/Shop.cs
+++ Assets/Scripts/UI/Phone/Shop.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Assets.SimpleLocalization.Scripts;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
     [SerializeField] private ShopList shopList;
     [SerializeField] private RectTransform canvas;
+    [SerializeField] private GameObject warning;
+    [SerializeField] private TextMeshProUGUI warningMessage;
     private List<Goods> listGoods;
     [SerializeField] private Transform packagePoint;
     int totalPrice = 0;
@@ -23,11 +28,27 @@ public class Shop : MonoBehaviour
     }
 
     private void OnButtonClick(int index, int totalPrice) {
-        //kasih tanda kalau player gak bisa beli, misalnya muncul warning
-        if (DeliveryManager.instance.CanCheckout() && CurrencyManager.instance.CanBuy(totalPrice) && !TimeManager.instance.NightHour())
+        if (!DeliveryManager.instance.CanCheckout())
         {
+            WarningMessage("Phone Storage Full");
+        } else if (!CurrencyManager.instance.CanBuy(totalPrice)) {
+            WarningMessage("Phone No Money");
+        } else if (TimeManager.instance.NightHour())
+        {
+            WarningMessage("Phone Cant Deliver");
+        } else {
+            WarningMessage("Phone Success");
             DeliveryManager.instance.StartDelivery(listGoods[index].prefab);
             CurrencyManager.instance.RemoveCurrency(totalPrice);
         }
+    }
+
+    private void WarningMessage(string message) {
+        warning.SetActive(true);
+        warningMessage.text = LocalizationManager.Localize(message);
+    }
+
+    public void CloseWarning() {
+        warning.SetActive(false);
     }
 }
