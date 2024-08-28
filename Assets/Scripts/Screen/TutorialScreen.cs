3576e78b6c3b4d6b9197032141c54f5d7d22d102
danish <danishalif16@gmail.com>
2024-08-28 14:07:51 +0700

    create tutorial menu

--- Assets/Scripts/Screen/TutorialScreen.cs
+++ Assets/Scripts/Screen/TutorialScreen.cs
@@ -1,67 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TutorialScreen : MonoBehaviour
 {
-    // [SerializeField] private float pageSpeed = 0.1f;
-    // [SerializeField] private List<Transform> pages;
-    // int currentIndex = -1;
-    // private bool rotate = false;
-    
-    // public void NextPage() {
-    //     if (rotate) return;
-    //     currentIndex++;
-    //     float angle= 180;
-    //     if (currentIndex >= pages.Count - 1)
-    //     {
-    //         return;
-    //     }
-    //     pages[currentIndex].SetAsLastSibling();
-    //     StartCoroutine(Rotate(angle, true));
-    // }
-
-    // public void PrevPage() {
-    //     if (rotate) return;
-    //     float angle = 0;
-    //     if (currentIndex -1 <= - 1)
-    //     {
-    //         return;
-    //     }
-    //     pages[currentIndex].SetAsLastSibling();
-    //     StartCoroutine(Rotate(angle, false));
-    // }
-
-    // private IEnumerator Rotate(float angle, bool forward) {
-    //     float value = 0;
-    //     while (true)
-    //     {
-    //         rotate = true;
-    //         Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
-    //         value += Time.deltaTime * pageSpeed;
-    //         pages[currentIndex].rotation = Quaternion.Slerp(pages[currentIndex].rotation, targetRotation, value);
-    //         float angle1 = Quaternion.Angle(pages[currentIndex].rotation, targetRotation);
-    //         if (angle1 < 0.1f)
-    //         {
-    //             if (!forward)
-    //             {
-    //                 currentIndex--;
-    //             }   
-    //             rotate = false;
-    //             break;
-    //         }
-    //     }
-    //     yield return null;
-    // }
-
     [SerializeField] private List<Transform> pages;
+    [SerializeField] private TextMeshProUGUI pageNumberText;
     [SerializeField] private Animator anim;
-    int currentIndex = 0;
+    [SerializeField] private AudioClip paperSound;
+    int currentIndex;
     Transform currentPage;
 
     private void OnEnable() {
-        currentPage = pages[0];
-        pages[0].gameObject.SetActive(true);
+        currentIndex = 0;
+        FlipPage();
     }
 
     public void NextPage() {
@@ -72,8 +25,9 @@ public class TutorialScreen : MonoBehaviour
         }
         
         currentIndex++;
+        FlipPage();
+        currentPage.gameObject.SetActive(false);
         anim.SetTrigger("next");
-        ShowPage();
     }
 
     public void PrevPage() {
@@ -84,14 +38,21 @@ public class TutorialScreen : MonoBehaviour
         }
 
         currentIndex--;
+        FlipPage();
+        currentPage.gameObject.SetActive(false);
         anim.SetTrigger("prev");
-        ShowPage();
     }
 
-    private void ShowPage() {
-        currentPage.gameObject.SetActive(false);
+    private void FlipPage() {
+        AudioManager.instance.PlaySound(paperSound);
+        int page = currentIndex +1;
+        pageNumberText.text = page + "/" + pages.Count;
+    }
+
+    public void ShowPage() {
         currentPage = pages[currentIndex];
         currentPage.gameObject.SetActive(true);
+        
     }
 
     private void OnDisable() {
